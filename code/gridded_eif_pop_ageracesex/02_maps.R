3392090bf3a0790a3e5ca043fab9aabf8f724018
Arnav Dharmagadda <arnav3d@outlook.com>
2025-06-04 16:24:05 -0400

    Update 02_maps.R
    Change Black, White, and Hispanic race maps. Include a continuous color distribution

--- code/gridded_eif_pop_ageracesex/02_maps.R
+++ code/gridded_eif_pop_ageracesex/02_maps.R
@@ -8,66 +8,46 @@
 # OUTPUTS: 
 ################################################################################
 
-tm_shape(ageracesex_2020) +
+#### RACE MAPS ####
+
+# Hispanic Population
+
+tm_shape(race_2023) +
   tm_polygons(col = "Hispanic", palette = "viridis",
               title = "Hispanic Population", lwd = 0,
-              style = "cont", breaks = seq(0, 300, 50),
+              style = "cont",
               lengend.hist = TRUE) +
   tm_layout(
-    main.title = "Gridded EIF Hispanic Population (2020)",  # ← key change
+    main.title = "Charlottesville, circa 2023", 
     main.title.position = "center",
-    outer.margins = c(0.05, 0, 0, 0),  # top, right, bottom, left
+    outer.margins = c(0.05, 0, 0, 0),
     legend.outside = TRUE
   )
 
-tm_shape(ageracesex_2020) +
+# White Population
+
+tm_shape(race_2023) +
+  tm_polygons(col = "White", palette = "viridis",
+              title = "White Population", lwd = 0,
+              style = "cont",
+              lengend.hist = TRUE) +
+  tm_layout(
+    main.title = "Charlottesville, circa 2023", 
+    main.title.position = "center",
+    outer.margins = c(0.05, 0, 0, 0),
+    legend.outside = TRUE
+  )
+
+# Black Population
+
+tm_shape(race_2023) +
   tm_polygons(col = "Black", palette = "viridis",
               title = "Black Population", lwd = 0,
-              style = "cont", breaks = seq(0, 500, 50),
+              style = "cont",
               lengend.hist = TRUE) +
   tm_layout(
-    main.title = "Gridded EIF Black Population (2020)",  # ← key change
+    main.title = "Charlottesville, circa 2023", 
     main.title.position = "center",
-    outer.margins = c(0.05, 0, 0, 0),  # top, right, bottom, left
-    legend.outside = TRUE
-  )
-
-tm_shape(ageracesex_2020) +
-  tm_polygons(col = "White", palette = "viridis",
-              title = "White Population", lwd = 0) +
-  tm_layout(
-    main.title = "Gridded EIF White Population (2020)",  # ← key change
-    main.title.position = "center",
-    outer.margins = c(0.05, 0, 0, 0),  # top, right, bottom, left
-    legend.outside = TRUE
-  )
-
-tm_shape(ageracesex_2020) +
-  tm_polygons(col = "Asian", palette = "viridis",
-              title = "Asian Population", lwd = 0) +
-  tm_layout(
-    main.title = "Gridded EIF Asian Population (2020)",  # ← key change
-    main.title.position = "center",
-    outer.margins = c(0.05, 0, 0, 0),  # top, right, bottom, left
-    legend.outside = TRUE
-  )
-
-tm_shape(ageracesex_2020) +
-  tm_polygons(col = "AIAN", palette = "viridis",
-              title = "Native American Population", lwd = 0) +
-  tm_layout(
-    main.title = "Gridded EIF Native American Population (2020)",  # ← key change
-    main.title.position = "center",
-    outer.margins = c(0.05, 0, 0, 0),  # top, right, bottom, left
-    legend.outside = TRUE
-  )
-
-tm_shape(ageracesex_2020) +
-  tm_polygons(col = "Other/Unknown", palette = "viridis",
-              title = "Other/Unknown Race", lwd = 0) +
-  tm_layout(
-    main.title = "Gridded EIF Other/Unknown Race Population (2020)",  # ← key change
-    main.title.position = "center",
-    outer.margins = c(0.05, 0, 0, 0),  # top, right, bottom, left
+    outer.margins = c(0.05, 0, 0, 0),
     legend.outside = TRUE
   )
