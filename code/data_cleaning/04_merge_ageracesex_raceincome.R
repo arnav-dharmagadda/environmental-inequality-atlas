45bbf4ff218db6f29ad613f1e44dd4c72a0f8dd9
Arnav Dharmagadda <arnav3d@outlook.com>
2025-06-10 16:56:56 -0400

    Update 04_merge_ageracesex_raceincome.R
    Renamed _x variables to remove the _x

--- code/data_cleaning/04_merge_ageracesex_raceincome.R
+++ code/data_cleaning/04_merge_ageracesex_raceincome.R
@@ -13,7 +13,7 @@
 
 ageracesex_income_year <- ageracesex_year_long %>%
   left_join(raceincome_year_long,   by = c("grid_lon", "grid_lat", "year")) %>%
-  rename_with(~ str_replace_all(., c("\\.x$" = "_x", "\\.y$" = "_y")))
+  rename_with(~ str_replace_all(., c("\\.x$" = "", "\\.y$" = "_y")))
 
 save(ageracesex_income_year, file = paste0(processed_path, "ageracesex_income_year.rda"))
 write_dta(ageracesex_income_year, paste0(processed_path, "ageracesex_income_year.dta"))
